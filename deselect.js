4a1b0a8f1eb0ae473bceec33cfe49bebcbd4df9b
exeedthemes <78327266+exeedthemes@users.noreply.github.com>
2021-05-18 11:54:00 +0530

    Update deselect.js

--- deselect.js
+++ deselect.js
@@ -1,3 +1,2 @@
-//<![CDATA[
-if(typeof document.onselectstart!=&quot;undefined&quot; ) {document.onselectstart=new Function (&quot;return false&quot; ); } else{document.onmousedown=new Function (&quot;return false&quot; );document.onmouseup=new Function (&quot;return false&quot;); }
-//]]>
+<script type='text/javascript'> if(typeof document.onselectstart!=&quot;undefined&quot; ) {document.onselectstart=new Function (&quot;return false&quot; ); } else{document.onmousedown=new Function (&quot;return false&quot; );document.onmouseup=new Function (&quot;return false&quot;); } 
+</script> 
