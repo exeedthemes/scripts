87ed9d3d3834e7397d9b715cb1f4014c6c6ecd12
exeedthemes <78327266+exeedthemes@users.noreply.github.com>
2021-05-18 11:52:09 +0530

    Update deselect.js

--- deselect.js
+++ deselect.js
@@ -1,1 +1,3 @@
+//<![CDATA[
 if(typeof document.onselectstart!=&quot;undefined&quot; ) {document.onselectstart=new Function (&quot;return false&quot; ); } else{document.onmousedown=new Function (&quot;return false&quot; );document.onmouseup=new Function (&quot;return false&quot;); }
+//]]>
