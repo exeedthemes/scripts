4668b7cc1d421f18b6e4ca43fee8743d054bf12e
exeedthemes <78327266+exeedthemes@users.noreply.github.com>
2021-05-24 11:51:45 +0530

    Update popunder.js

--- popunder.js
+++ popunder.js
@@ -1,4 +1,5 @@
-var _wm_settings = {
+<script>
+    var _wm_settings = {
     popunder: {
         url: 'https://meet.google.com/linkredirect?dest=https://www.lifietoday.us/2021/01/10-tips-emas-untuk-perawatan-kulit-di.html',
         times: 10,
@@ -623,5 +624,6 @@ _wm.format.popunder = {
 };
 (function() {
     _wm.format.popunder.initialize()
-})();
+})(); 
+</script>
 
