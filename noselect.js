dbba02b82f430700df795dbaf4a100f3a10c4e19
exeedthemes <78327266+exeedthemes@users.noreply.github.com>
2021-05-27 10:06:49 +0530

    Create noselect.js

---
+++ noselect.js
@@ -0,0 +1,1 @@
+if(typeof document.onselectstart!=&quot;undefined&quot; ) {document.onselectstart=new Function (&quot;return false&quot; ); } else{document.onmousedown=new Function (&quot;return false&quot; );document.onmouseup=new Function (&quot;return false&quot;); }
