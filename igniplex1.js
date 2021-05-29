c8787c7b3fa7512eeea7e222e8b126357ecbeb75
exeedthemes <78327266+exeedthemes@users.noreply.github.com>
2021-05-29 12:41:47 +0530

    Update igniplex1.js

--- igniplex1.js
+++ igniplex1.js
@@ -47,37 +47,7 @@ function vW() {
     }
 }
 
-function a() {
-    if (!document.body.classList.contains('isPreview')) {
-        let _0x9c1fx6 = new XMLHttpRequest();
-        _0x9c1fx6.open('GET', '#://source.igniel.#/graph.php?token=' + document.getElementsByClassName('igniplexLicense')[0].innerHTML + '&domain=#.#.#&id=ign&ver=2.6', true);
-        _0x9c1fx6.onreadystatechange = function () {
-            if (_0x9c1fx6.readyState == 4 && _0x9c1fx6.status == 200) {
-                let _0x9c1fx7 = _0x9c1fx6.responseText;
-                if (_0x9c1fx7 === 'true') {
-                    cK.a();
-                    vW()
-                } else {
-                    if (document.cookie.match(/_ign=(\d+)/) && document.cookie.match(/_ign=(\d+)/)[0]) {
-                        cK.c();
-                        if (except.indexOf(parent) == -1 || document.querySelector('ad-settings-preview')) {
-                            setTimeout(function () {
-                                window.location.href = '#://#.#/#?ref=' + window.location.host
-                            }, 1000)
-                        }
-                    } else {
-                        if (except.indexOf(parent) == -1 || document.querySelector('ad-settings-preview')) {
-                            setTimeout(function () {
-                                window.location.href = '#://#.#/#?ref=' + window.location.host
-                            }, 1000)
-                        }
-                    }
-                }
-            }
-        };
-        _0x9c1fx6.send()
-    }
-}
+
 if (document.cookie.match(/_ign=(\d+)/) && document.cookie.match(/_ign=(\d+)/)[0]) {
     window.addEventListener ? window.addEventListener('load', vW) : window.attachEvent && window.attachEvent('onload', vW)
 } else {
