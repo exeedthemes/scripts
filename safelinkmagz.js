0a1727c92e65b13dbd5d22121854d3abab18624e
exeedthemes <78327266+exeedthemes@users.noreply.github.com>
2021-05-29 09:04:13 +0530

    Update safelinkmagz.js

--- safelinkmagz.js
+++ safelinkmagz.js
@@ -1510,3 +1510,25 @@ $.ajax({
     },
     'async': true
 });
+
+  if (adblockJkoding=='on'){
+document.addEventListener("DOMContentLoaded", function() {
+var AdblockJkoding = document.createElement("div");
+AdblockJkoding.innerHTML=`<div id='areaScriptABlock'></div>`;
+AdblockJkoding.id= 'myMessage';
+document.body.append(AdblockJkoding);
+NextAddPanelDetecAdBlock();
+});
+function NextAddPanelDetecAdBlock() {
+  var judulAd = 'AdBlock Detected!!<br/>Matikan AdBlock';
+  var notifAd = 'Agar blog tetap berjalan, matikan AdBlock.<br/>Terima kasih.'; 
+  var _0xc9cbx2 = document.createElement("div");
+  _0xc9cbx2.id = "jkodingAdBlock";
+  _0xc9cbx2.innerHTML = '<div class="isiAds"><span class="judul">' + judulAd + '</span><br/><svg viewBox="0 0 24 24"><path d="M13,13H11V7H13M12,17\.3A1\.3,1\.3 0 0,1 10\.7,16A1\.3,1\.3 0 0,1 12,14\.7A1\.3,1\.3 0 0,1 13\.3,16A1\.3,1\.3 0 0,1 12,17\.3M15\.73,3H8\.27L3,8\.27V15\.73L8\.27,21H15\.73L21,15\.73V8\.27L15\.73,3Z"></path></svg><br/>' + notifAd + "</div>";
+   setTimeout(function(){
+	if(document.getElementById('myMessage')) {
+	document.getElementById('myMessage').append(_0xc9cbx2);
+};
+   },1000);
+  };
+  };
