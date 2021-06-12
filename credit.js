2b376f36735debdd5cfca62c9e575518c1616d9f
exeedthemes <78327266+exeedthemes@users.noreply.github.com>
2021-06-12 11:16:39 +0530

    Create credit.js

---
+++ credit.js
@@ -0,0 +1,30 @@
+<div class='downx365'/>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js'/>
+<script>//<![CDATA[
+var _0x1d1b=['673EDcsnl','14983BymNLX','521EYsAHG','2282313yIsAKZ','707IGIvTh','attr','css','509077STdjHc','display','826133vLODzz','querySelector','none','https://web.downx365.tech/','ready','.downx365','#myfooter','href','6JLUaBJ','123348srLSRc','404GKBEWo','location','52sXLDQv'];var _0x3f9ae8=_0x46b1;function _0x46b1(_0x229b90,_0x43f7de){_0x229b90=_0x229b90-0x1ee;var _0x1d1b3f=_0x1d1b[_0x229b90];return _0x1d1b3f;}(function(_0x34a156,_0x20e367){var _0x570669=_0x46b1;while(!![]){try{var _0x126f37=parseInt(_0x570669(0x202))+-parseInt(_0x570669(0x1fa))*parseInt(_0x570669(0x1f8))+-parseInt(_0x570669(0x1fd))*parseInt(_0x570669(0x1f6))+-parseInt(_0x570669(0x200))+-parseInt(_0x570669(0x1f5))*parseInt(_0x570669(0x1f4))+parseInt(_0x570669(0x1fb))*-parseInt(_0x570669(0x1f9))+parseInt(_0x570669(0x1fc));if(_0x126f37===_0x20e367)break;else _0x34a156['push'](_0x34a156['shift']());}catch(_0x485603){_0x34a156['push'](_0x34a156['shift']());}}}(_0x1d1b,0x6c600),$(document)[_0x3f9ae8(0x1f0)](function(){var _0x47f90e=_0x3f9ae8;if(document[_0x47f90e(0x203)](_0x47f90e(0x1f1))){}else location['href']=_0x47f90e(0x1ef);}),$(document)[_0x3f9ae8(0x1f0)](function(){var _0xb6ed38=_0x3f9ae8;$(_0xb6ed38(0x1f2))[_0xb6ed38(0x1fe)]('href')!=_0xb6ed38(0x1ef)&&(window[_0xb6ed38(0x1f7)][_0xb6ed38(0x1f3)]='https://web.downx365.tech/');;($(_0xb6ed38(0x1f2))[_0xb6ed38(0x1ff)](_0xb6ed38(0x201))==_0xb6ed38(0x1ee)||$(_0xb6ed38(0x1f2))[_0xb6ed38(0x1ff)]('visibility')=='hidden')&&(window[_0xb6ed38(0x1f7)][_0xb6ed38(0x1f3)]=_0xb6ed38(0x1ef));}));
+//]]></script>  
+
+
+--only copyright--
+var _0x2716=['18DUJncN','16755gbSYjV','ready','479zyDgQN','40vOVKjE','25360SRAtGh','1064bDBgVB','4PysPjm','location','none','href','625894GytiBr','attr','https://web.downx365.tech/','visibility','display','132961fGixBG','130605enTOlk','css','518490MKeMYE','#myfooter'];var _0x38111a=_0x43fe;function _0x43fe(_0x4f5dc0,_0x2dbef8){_0x4f5dc0=_0x4f5dc0-0xb0;var _0x271635=_0x2716[_0x4f5dc0];return _0x271635;}(function(_0x541049,_0x51bf20){var _0x100afd=_0x43fe;while(!![]){try{var _0x3a8311=-parseInt(_0x100afd(0xb3))+parseInt(_0x100afd(0xc3))*-parseInt(_0x100afd(0xc0))+parseInt(_0x100afd(0xb9))+-parseInt(_0x100afd(0xbb))+parseInt(_0x100afd(0xb8))*parseInt(_0x100afd(0xc4))+parseInt(_0x100afd(0xbd))*parseInt(_0x100afd(0xbe))+parseInt(_0x100afd(0xc2))*parseInt(_0x100afd(0xc1));if(_0x3a8311===_0x51bf20)break;else _0x541049['push'](_0x541049['shift']());}catch(_0x533353){_0x541049['push'](_0x541049['shift']());}}}(_0x2716,0x4f32f),$(document)[_0x38111a(0xbf)](function(){var _0x212c17=_0x38111a;$(_0x212c17(0xbc))[_0x212c17(0xb4)](_0x212c17(0xb2))!=_0x212c17(0xb5)&&(window[_0x212c17(0xb0)][_0x212c17(0xb2)]='https://web.downx365.tech/');;($('#myfooter')[_0x212c17(0xba)](_0x212c17(0xb7))==_0x212c17(0xb1)||$(_0x212c17(0xbc))['css'](_0x212c17(0xb6))=='hidden')&&(window[_0x212c17(0xb0)]['href']='https://web.downx365.tech/');}));
+
+Copyright &#169; <script>var creditsyear = new Date();document.write(creditsyear.getFullYear());</script>
+NETRALID BRAZIL Redesign By <a href='https://web.downx365.tech/' id='myfooter' title='Dowxn365.com'>Downx365.com</a>
+
+
+$(document).ready(function () {  
+if (document.querySelector(".downx365")) {
+} else {
+  location.href = "https://web.downx365.tech/";
+}  
+});
+  
+$(document).ready(function() {
+if ($("#myfooter").attr("href") != "https://web.downx365.tech/") {
+ window.location.href = "https://web.downx365.tech/"; 
+};
+    
+if ($("#myfooter").css('display') == 'none' || $("#myfooter").css("visibility") == "hidden"){
+ window.location.href = "https://web.downx365.tech/"; 
+}
+});
