330fa0488a6bc3a93f67c938a870529dd057fb5b
exeedthemes <78327266+exeedthemes@users.noreply.github.com>
2021-06-20 10:48:06 +0530

    Update htmlunparser.js

--- htmlunparser.js
+++ htmlunparser.js
@@ -2,15 +2,15 @@ function convert() {
     var ele1 = document.getElementById("somewhere1");
     var replaced;
     replaced = ele1.value;
-    replaced = replaced.replace("&amp;" , /&/ig);
-    replaced = replaced.replace("&lt;" , /</ig);
-    replaced = replaced.replace("&gt;" , />/ig);
-    replaced = replaced.replace("&quot;" , /"/ig);
-    replaced = replaced.replace("&#039;" , /'/ig);
-    replaced = replaced.replace("&plusmn;" , /&#177;/ig);
-    replaced = replaced.replace("&copy;" , /&#169;/ig);
-    replaced = replaced.replace("&reg;" , /&#174;/ig);
-    replaced = replaced.replace("ya'll" , /ya'll/ig);
+    replaced = replaced.replace("&amp;", /&/ig);
+    replaced = replaced.replace("&lt;", /</ig);
+    replaced = replaced.replace("&gt;", />/ig);
+    replaced = replaced.replace("&quot;", /"/ig);
+    replaced = replaced.replace("&#039;", /'/ig);
+    replaced = replaced.replace("&plusmn;", /&#177;/ig);
+    replaced = replaced.replace("&copy;", /&#169;/ig);
+    replaced = replaced.replace("&reg;", /&#174;/ig);
+    replaced = replaced.replace("ya'll", /ya'll/ig);
     ele1.value = replaced;
 	document.getElementById("button-link1")
         .style.display = "inline-block";
