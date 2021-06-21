2c54e8fef43eeed29e68c90232a2a31fbe066c72
exeedthemes <78327266+exeedthemes@users.noreply.github.com>
2021-06-21 17:19:11 +0530

    Create videopopup.js

---
+++ videopopup.js
@@ -0,0 +1,5 @@
+document.body.addEventListener('click', myFunction);
+    function myFunction() {
+window.open('<--videolink-->', 'poppage', 'toolbars=0, scrollbars=1, location=0, statusbars=0, menubars=0, resizable=1, width=100, height=100, left = 1200, top = 500');
+ document.body.removeEventListener('click', myFunction);
+    }
