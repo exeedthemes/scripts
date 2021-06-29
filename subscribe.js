1cdf7d1ae9be3df87476d99a6c7ff59dfbfa4b75
exeedthemes <78327266+exeedthemes@users.noreply.github.com>
2021-06-29 06:46:14 +0530

    Create subscribe.js

---
+++ subscribe.js
@@ -0,0 +1,57 @@
+<style>
+.downx365-sub {
+    -webkit-animation: bounce 2s infinite;
+    animation: bounce 2s infinite;
+}
+
+/* Scroll down indicator (bouncing) */
+@-webkit-keyframes bounce {
+  0%, 10%, 20%, 30%, 50% {
+    -webkit-transform: translateY(0); }
+  40% {
+    -webkit-transform: translateY(-30px); }
+  60% {
+    -webkit-transform: translateY(-15px); } }
+@-moz-keyframes bounce {
+  0%, 20%, 50%, 80%, 100% {
+    -moz-transform: translateY(0); }
+  40% {
+    -moz-transform: translateY(-30px); }
+  60% {
+    -moz-transform: translateY(-15px); } }
+@keyframes bounce {
+  0%, 20%, 50%, 80%, 100% {
+    -webkit-transform: translateY(0);
+    -moz-transform: translateY(0);
+    -ms-transform: translateY(0);
+    -o-transform: translateY(0);
+    transform: translateY(0); }
+  40% {
+    -webkit-transform: translateY(-30px);
+    -moz-transform: translateY(-30px);
+    -ms-transform: translateY(-30px);
+    -o-transform: translateY(-30px);
+    transform: translateY(-30px); }
+  60% {
+    -webkit-transform: translateY(-15px);
+    -moz-transform: translateY(-15px);
+    -ms-transform: translateY(-15px);
+    -o-transform: translateY(-15px);
+    transform: translateY(-15px); } }
+
+.downx365-sub { position: fixed; left: 55px; bottom: 20px;  background-color: #2e2e2e; padding: 12px 15px 12px 12px; border-radius: 15px; box-shadow: 0 4px 12px 0 rgba(9,32,76,.05); display: flex; align-items: center; max-width: 300px; min-width: 280px; color: #000000; overflow: hidden; z-index: 50;} .downx365-sub .downx365-sub-close { position: absolute; top: 4px; right: 8px;} .downx365-sub .downx365-sub-img { width: 70px; height: 50px; } .downx365-sub .downx365-sub-img img { display: flex; align-items: center; justify-content: center; width: 50px; } .downx365-sub .downx365-sub-content { width: calc(100% - 70px); padding-left: 1px; } .downx365-sub .downx365-sub-content a { display: block; color: inherit; } .downx365-btn{font-weight: 700; font-size: 11px; display: flex; align-items: center; justify-content: flex-end; width: 73px; height: 25px; padding-right: 12px; background-color: #e4393c; border-radius: 4px; color: #fff; margin-top: 12px; bottom: 0; right: 0;} .downx365-sub .downx365-sub-content .title { display: block; font-weight: 700; font-size: 13px; color:#fff; text-align: left; padding-left: 12px}.downx365-sub .downx365-btn a { display: block; color: inherit; color:#fff; } .downx365-sub .downx365-sub-content .desc { display: block; font-size: 10px; margin-top: 5px; color: #fff; text-align: left;padding-left: 12px }.bt-text{margin-left:9px}#close {position: relative; float: right; background: #e4393c; color: #f2f2f2; top: 0px; right: 0px; border-radius: 50%; cursor: pointer; 
+font-size: 8px; border:none;}
+</style>
+
+<div class='downx365-sub' id='subscribe'>
+<div class='downx365-sub-close' onclick='document.getElementById("subscribe").style.display="none"'><button id = "close">X</button></div>
+<div class='downx365-sub-img'><img alt='piymanhackdat' src='https://1.bp.blogspot.com/-ZSlQGGsdAxY/YLyINuRJvKI/AAAAAAAAOac/CY3IIEeuULgDcxJmN5mq3rYTyigjLskowCNcBGAsYHQ/s320/unnamed.jpg'/>
+</div>
+<div class='downx365-sub-content'>
+<span class='title'>PIYMAN HACKDAT</span>
+<span class='desc'>Subscribe Our Youtube Channel</span>
+</div>
+<div class='downx365-btn'>
+<a href='https://www.youtube.com/channel/UC2DfT9IrjMzAL1AH3U6Wu7w?sub_confirmation=true' target='_blank'>
+<span class='bt-text'>Subscribe</span></a>
+</div></div>
