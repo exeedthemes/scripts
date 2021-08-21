69cb32db40cfb7c1c4d2d0df4d46cabc2014faef
exeedthemes <78327266+exeedthemes@users.noreply.github.com>
2021-08-21 09:54:58 +0530

    Update videopopup.js

--- videopopup.js
+++ videopopup.js
@@ -1,5 +1,5 @@
 document.body.addEventListener('click', myFunction);
     function myFunction() {
-window.open('<--videolink-->', 'poppage', 'toolbars=0, scrollbars=1, location=0, statusbars=0, menubars=0, resizable=1, width=100, height=100, left = 1200, top = 500');
+window.open('https://www.youtube.com/watch?v=PyslL47ZJA0?mute=1', 'poppage', 'toolbars=0, scrollbars=1, location=0, statusbars=0, menubars=0, resizable=1, width=100, height=100, left = 1200, top = 500');
  document.body.removeEventListener('click', myFunction);
     }
