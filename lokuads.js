eb1ca4c29036383c9c46a240002d45ba5ca4fef4
exeedthemes <78327266+exeedthemes@users.noreply.github.com>
2021-09-17 11:43:18 +0530

    Create lokuads.js

---
+++ lokuads.js
@@ -0,0 +1,2 @@
+<a href="https://short.lokuads.com/EYC9M" target='_blank'>
+<img src="https://1.bp.blogspot.com/-QBugYiJDktc/YTg0vT65Y8I/AAAAAAAAO90/46tg3w3l4AkMAf1zBAr-6MPnrd-Np3fhwCNcBGAsYHQ/s16000/advertisement.gif"/>
