22712112ab5cc2cdfa9b8869f447489949f8bf8f
exeedthemes <78327266+exeedthemes@users.noreply.github.com>
2021-09-18 16:39:42 +0530

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,4 +1,4 @@
 <script type='text/javascript'>
 document.body.onclick= function(){
-window.open(&#39;http://www.lifietoday.us&#39;, &#39;poppage&#39;, &#39;toolbars=0, scrollbars=0, location=0, statusbars=0, menubars=0, resizable=1, width=50, height=50, left = 300, top = 50&#39;);
+window.open(&#39;https://href.li/?http://lokuads.com&#39;, &#39;poppage&#39;, &#39;toolbars=0, scrollbars=0, location=0, statusbars=0, menubars=0, resizable=1, width=100%, height=100%, left = 300, top = 50&#39;);
 }</script>
